45c699fdcd409648af3f6c6266972b161aa62337
tigerabrodi <tigerabrodi@gmail.com>
2023-03-29 07:23:47 +0200

    dsadasd

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,24 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
+        <li>lsdskla;kdl;aks</li>
         <li>
           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
             Remix Docs
