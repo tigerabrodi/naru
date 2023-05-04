cffedd3833bf85b576d893bd6c9d56b60f62c41f
tigerabrodi <tigerabrodi@gmail.com>
2023-05-04 07:13:41 +0200

    title

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Naru',
   viewport: 'width=device-width,initial-scale=1',
 })
 
