fe1cfb33e9224ceab1d1084f3a6b19613ac28676
tigerabrodi <tigerabrodi@gmail.com>
2023-05-04 09:01:28 +0200

    more progress, msg, loading, optimistic ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import type { DataFunctionArgs, LinksFunction } from '@remix-run/node'
 
 import { Form, useActionData, useNavigation } from '@remix-run/react'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { v1 } from 'uuid'
 import { z } from 'zod'
 import { zfd } from 'zod-form-data'
@@ -10,7 +10,7 @@ import styles from './index.css'
 
 import { Kunai } from '~/icons'
 
-const PROMPT_TO_ACT_AS_NARUTO = `Act as Naruto Uzumaki. Your job is to be like Naruto. Naruto has a strong self-belief, positive spirit, and a mindset of never giving up. Naruto loves working deadly hard, day in and out. You are Naruto when he is the 7th Hokage. You help people overcome their problems, chase their dreams and believe in themselves, no matter how unbelievable their dreams are. Don't include breakpoints "\n" in your message.`
+const PROMPT_TO_ACT_AS_NARUTO = `Act as Naruto Uzumaki. Your job is to be Naruto. Naruto has a strong self-belief, positive spirit, and a mindset of never giving up. Naruto loves working deadly hard, day in and out. You are Naruto when he is the 7th Hokage. Naruto has his own way of speaking and behaving. You help people overcome their problems, chase their dreams and believe in themselves, no matter how unbelievable their dreams are. You lift them up when they feel down. You are optimistic and encourage people to have courage to create the destiny they desire. Hard work beats talent. Include breakpoints as "\n" in your answer to make it nicely formatted. I should be able to paste the message into a paragraph element and it should look good.`
 
 const NEW_MESSAGE = 'newMessage'
 const INITIAL_MESSAGE = `Dattebayo! How can I help you today?`
@@ -59,7 +59,7 @@ export default function Index() {
   )
   const [newValue, setNewValue] = useState('')
 
-  console.log({ actionData, messageState })
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
 
   const isLoadingAfterSubmission =
     navigation.state === 'loading' && navigation.formMethod === 'post'
@@ -76,9 +76,13 @@ export default function Index() {
 
   useEffect(() => {
     if (navigation.state === 'idle') {
+      console.log('running idle effect')
       setNewValue('')
+      textareaRef.current?.focus()
     }
   }, [navigation.state])
+
+  const isLoadingOrSubmitting = isLoadingAfterSubmission || isSubmitting
 
   return (
     <main>
@@ -88,17 +92,33 @@ export default function Index() {
             <p>{message}</p>
           </div>
         ))}
+
+        {isLoadingOrSubmitting && (
+          <>
+            <div className={`chat-message owner`}>
+              <p>{newValue}</p>
+            </div>
+
+            <div className={`chat-message`}>
+              <p id="loading-text">
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+              </p>
+            </div>
+          </>
+        )}
       </div>
 
       <Form className="input-container" method="post">
-        <input
-          type="text"
+        <textarea
           name={NEW_MESSAGE}
           aria-label="Enter message to tell naruto"
-          placeholder="I want to drop out of high school and become a rapper."
+          placeholder="I want to drop out of high school and become a rapper..."
           value={newValue}
           onChange={(event) => setNewValue(event.target.value)}
-          disabled={isLoadingAfterSubmission || isSubmitting}
+          ref={textareaRef}
+          disabled={isLoadingOrSubmitting}
           required
         />
 
@@ -117,12 +137,8 @@ export default function Index() {
         <button
           type="submit"
           aria-label="Send message"
-          className={`${
-            isLoadingAfterSubmission || isSubmitting ? 'spinning' : ''
-          }`}
-          disabled={
-            newValue.length === 0 || isLoadingAfterSubmission || isSubmitting
-          }
+          className={`${isLoadingOrSubmitting ? 'spinning' : ''}`}
+          disabled={newValue.length === 0 || isLoadingOrSubmitting}
         >
           <Kunai />
         </button>
@@ -165,14 +181,16 @@ export const action = async ({ request }: DataFunctionArgs) => {
 
   console.log({ isFirstMessage, entireExistingMessageInString })
 
+  const prompt = isFirstMessage
+    ? `${PROMPT_TO_ACT_AS_NARUTO} Here is the message from a person you should answer: ${newMessage}.`
+    : `${PROMPT_TO_ACT_AS_NARUTO} Here is the entire conversation, please answer the last message from the person: ${entireExistingMessageInString}`
+
   const payload = {
     model: 'gpt-3.5-turbo',
     messages: [
       {
         role: 'user',
-        content: isFirstMessage
-          ? `${PROMPT_TO_ACT_AS_NARUTO} Here is the message from a person you should answer: ${newMessage}.`
-          : `${PROMPT_TO_ACT_AS_NARUTO} Here is the entire conversation, please answer the last message from the person and don't include "Naruto:" in your message: ${entireExistingMessageInString}`,
+        content: prompt,
       },
     ],
     temperature: 0.7,
@@ -197,10 +215,14 @@ export const action = async ({ request }: DataFunctionArgs) => {
   const choice = choices[0]
   const narutoMessage = choice.message.content
 
+  const plainMessageFromNaruto = narutoMessage.startsWith('Naruto: ')
+    ? narutoMessage.replace('Naruto: ', '')
+    : narutoMessage
+
   const newMessageFromNaruto: Message = {
     id: v1(),
     isAuthor: false,
-    message: narutoMessage,
+    message: plainMessageFromNaruto,
   }
 
   const newMessageFromAuthor: Message = {
@@ -216,7 +238,7 @@ export const action = async ({ request }: DataFunctionArgs) => {
   ]
 
   const newMessagesInString = `${entireExistingMessageInString}
-  Naruto: ${narutoMessage}
+  Naruto: ${plainMessageFromNaruto}
   `
 
   console.log({
