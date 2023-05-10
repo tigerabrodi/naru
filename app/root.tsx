d77f1f9cd7ede32e67312815b87445db17e5eb0b
tigerabrodi <tigerabrodi@gmail.com>
2023-05-10 07:54:00 +0200

    add meta

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,25 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'Naru',
+  description: 'Chat with naruto and get real fucking advice.',
   viewport: 'width=device-width,initial-scale=1',
+  keywords:
+    'remix,naruto,chat,narutouzumaki,hokage,advice,selfimprovement,dreams,success',
+  'og:title': 'Naru',
+  'og:type': 'website',
+  'og:url': 'https://naru.chat/',
+  'og:image':
+    'https://user-images.githubusercontent.com/49603590/237285360-40d13fc8-3e85-409f-a520-df15c710e5c2.png',
+  'og:card': 'summary_large_image',
+  'og:creator': '@tabrodi',
+  'og:site': 'https://naru.chat/',
+  'og:description': 'Chat with naruto and get real fucking advice.',
+  'twitter:image':
+    'https://user-images.githubusercontent.com/49603590/237285360-40d13fc8-3e85-409f-a520-df15c710e5c2.png',
+  'twitter:card': 'summary_large_image',
+  'twitter:creator': '@tabrodi',
+  'twitter:title': 'Naru',
+  'twitter:description': 'Chat with naruto and get real fucking advice.',
 })
 
 export default function App() {
